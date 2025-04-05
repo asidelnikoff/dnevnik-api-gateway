3711715bcb37248fad50ea846701b188ebce0657
Александр Сидельников <alex.sidelnikov03@gmail.com>
2025-04-05 17:34:09 +0300

    Добавлены заглушки роутов работы с оценками

---
+++ src/Dnevnik.ApiGateway/Controllers/Dto/Requests/CreateGrade.cs
@@ -0,0 +1,7 @@
+﻿namespace Dnevnik.ApiGateway.Controllers.Dto.Requests;
+
+public class CreateGrade
+{
+    public Guid ScheduleId { get; init; }
+    public required StudentGrade[] Grades { get; init; }
+}
---
+++ src/Dnevnik.ApiGateway/Controllers/GradesController.cs
@@ -0,0 +1,14 @@
+﻿using Dnevnik.ApiGateway.Controllers.Dto.Requests;
+
+using Microsoft.AspNetCore.Mvc;
+
+namespace Dnevnik.ApiGateway.Controllers;
+
+public class GradesController : BaseController
+{
+    [HttpPost("grades")]
+    public IActionResult PostGrades(CreateGrade request) => throw new NotImplementedException();
+
+    [HttpPut("grades")]
+    public IActionResult UpdateGrades(CreateGrade request) => throw new NotImplementedException();
+}

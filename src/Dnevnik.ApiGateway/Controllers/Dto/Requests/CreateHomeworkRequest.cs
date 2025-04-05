9e3ec2a90434936b042d19296538e40ceab152ac
Александр Сидельников <alex.sidelnikov03@gmail.com>
2025-04-05 17:38:09 +0300

    Добавлены заглушки роутов работы с дз

---
+++ src/Dnevnik.ApiGateway/Controllers/Dto/Requests/CreateHomeworkRequest.cs
@@ -0,0 +1,7 @@
+﻿namespace Dnevnik.ApiGateway.Controllers.Dto.Requests;
+
+public class CreateHomeworkRequest
+{
+    public Guid ScheduleId { get; init; }
+    public required string Homework { get; init; }
+}
---
+++ src/Dnevnik.ApiGateway/Controllers/HomeworkController.cs
@@ -0,0 +1,17 @@
+﻿using Dnevnik.ApiGateway.Controllers.Dto.Requests;
+
+using Microsoft.AspNetCore.Mvc;
+
+namespace Dnevnik.ApiGateway.Controllers;
+
+public class HomeworkController : BaseController
+{
+    [HttpPost("homework")]
+    public IActionResult CreateHomework(CreateHomeworkRequest request) => throw new NotImplementedException();
+
+    [HttpPut("homework")]
+    public IActionResult UpdateHomework(CreateHomeworkRequest request) => throw new NotImplementedException();
+
+    [HttpDelete("homework/{id}")]
+    public IActionResult DeleteHomework(Guid id) => throw new NotImplementedException();
+}

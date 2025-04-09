4ca3366d3084e7033b25a9c69dd29a655ecf6a8b
Александр Сидельников <sanek0934@gmail.com>
2025-04-09 13:34:31 +0300

    Метод внесен в интерфейс

--- src/Dnevnik.ApiGateway/Services/HttpService/IHttpService.cs
+++ src/Dnevnik.ApiGateway/Services/HttpService/IHttpService.cs
@@ -5,4 +5,5 @@ public interface IHttpService
     Task<string> PostAsync(HttpPostRequest request);
     Task<string> GetAsync(BaseHttpRequest request);
     Task<string> DeleteAsync(BaseHttpRequest request);
+    Task<string> PutAsync(HttpPostRequest request);
 }

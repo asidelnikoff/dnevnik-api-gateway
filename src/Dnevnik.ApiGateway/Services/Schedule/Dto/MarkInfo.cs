392df43355da9284e182fe49bc14326e7088cfab
Александр Сидельников <sanek0934@gmail.com>
2025-04-26 14:41:33 +0300

    Доработка взаимодействия с микросервсиом расписания

--- src/Dnevnik.ApiGateway/Services/Schedule/Dto/MarkInfo.cs
+++ src/Dnevnik.ApiGateway/Services/Schedule/Dto/MarkInfo.cs
@@ -4,7 +4,8 @@ public class MarkInfo
 {
     public DateOnly Date { get; init; }
     public Guid TeacherId { get; init; }
-    public Guid UserId { get; init; }
+    public Guid StudentId { get; init; }
     public Guid LessonId { get; init; }
-    public string? Mark { get; init; }
+    public int? Mark { get; init; }
+    public string? Comment { get; init; }
 }
